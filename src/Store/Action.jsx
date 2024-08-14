936d57711a09cccf6f52f7ccf6b9373f35fb795c
Ritikjain91 <jainritik829@gmail.com>
2024-08-14 21:39:23 +0530

    todo list app using redux

---
+++ src/Store/Action.jsx
@@ -0,0 +1,9 @@
+export const addTodo = (todo) => ({
+    type: 'ADD_TODO',
+    payload: todo
+  });
+  
+  export const removeTodo = (index) => ({
+    type: 'REMOVE_TODO',
+    payload: index
+  });
---
+++ src/Store/Store.jsx
@@ -0,0 +1,7 @@
+// src/store/store.js
+import { createStore } from 'redux';
+import todoReducer from './todoReducer';
+
+const store = createStore(todoReducer);
+
+export default store;
